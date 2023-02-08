c41bd915e9032c933b186987d786e64f406e31e4
Paul Henderson <paul@pmh47.net>
2023-02-08 16:03:25 +0000

    Switching to scikit-learn for validation LR accuracy; making regularisation optional

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ import torch.nn.functional as F
 import torchvision.datasets
 import torchvision.models
 import pytorch_lightning as pl
+import sklearn.linear_model
 
 
-def get_logistic_regression_loss(embedding, labels, num_iterations=20):
+def get_logistic_regression_loss(embedding, labels, num_iterations=100):
     # embedding :: iib, channel -> float32
     # labels :: iib -> int
     num_classes = 10
@@ -25,16 +26,27 @@ def get_logistic_regression_loss(embedding, labels, num_iterations=20):
                 param.data -= lr * grad
     final_logits, final_loss = logits_and_loss()
     final_accuracy = (final_logits.argmax(dim=1) == labels).float().mean()
-    # print('final loss:', final_loss.item(), '; final accuracy:', (final_logits.argmax(dim=1) == labels).float().mean().item())
-    # we want the grad of the after-final-iteration loss, wrt the parameters of the neural network -- and thus of the embedding
-    # print('G', torch.autograd.grad(current_loss, embedding, create_graph=False))
     return final_loss, final_accuracy
+
+
+def get_logistic_regression_accuracy_skl(embedding, labels, max_num_iterations=2000):
+    # embedding :: iib, channel -> float32
+    # labels :: iib -> int
+    num_classes = 10
+    embedding, labels = embedding.cpu().numpy(), labels.cpu().numpy()
+    regressor = sklearn.linear_model.LogisticRegression(random_state=0, max_iter=max_num_iterations)
+    regressor.fit(embedding, labels)
+    assert len(regressor.classes_) == num_classes
+    predictions = regressor.predict(embedding)
+    accuracy = (predictions == labels).mean()
+    return accuracy
 
 
 class Model(pl.LightningModule):
 
-    def __init__(self, hidden_size=128, alphabet_size=27):
+    def __init__(self, lr_regularised, hidden_size=128, alphabet_size=27):
         super().__init__()
+        self.lr_regularised = lr_regularised
         self.resnet = torchvision.models.resnet18(num_classes=128)
         self.fc = nn.Sequential(
             nn.ELU(),
@@ -53,7 +65,14 @@ class Model(pl.LightningModule):
     def training_step(self, batch, batch_idx):
         images, ordinal_labels, text_labels = batch
         embedding = self.fc(self.resnet(images.expand(-1, 3, -1, -1)))
-        lr_loss, _ = get_logistic_regression_loss(embedding, ordinal_labels)
+        if self.lr_regularised:
+            lr_loss, _ = get_logistic_regression_loss(embedding, ordinal_labels)
+        else:
+            lr_loss = 0
+        # want to apply the above to 'many' layers
+        # note for mnist, logistic-regression *on the pixels* yields about 95.5% accuracy! it follows that LR on any early conv layers will also work well
+        # hence, only consider dense (or layer conv) layers
+        # could either do per-layer, or could consider the concatenation of all activations --> the latter feels somehow more convincing
         character_logits = self.char_decoder(embedding.unsqueeze(-1))  # iib, char-in-alphabet, char-in-seq
         classification_loss = F.cross_entropy(character_logits, text_labels)
         return classification_loss + -lr_loss
@@ -61,7 +80,6 @@ class Model(pl.LightningModule):
     def validation_step(self, batch, batch_idx):
         images, ordinal_labels, text_labels = batch
         embedding = self.fc(self.resnet(images.expand(-1, 3, -1, -1)))
-        _, lr_accuracy = get_logistic_regression_loss(embedding, ordinal_labels, num_iterations=100)
         character_logits = self.char_decoder(embedding.unsqueeze(-1))  # iib, char-in-alphabet, char-in-seq
         loss = F.cross_entropy(character_logits, text_labels)
         char_indices = torch.argmax(character_logits, dim=1)
@@ -71,7 +89,17 @@ class Model(pl.LightningModule):
         self.log("val_loss", loss, prog_bar=True, on_step=False, on_epoch=True)
         self.log("val_charwise_accuracy", charwise_accuracy, prog_bar=True, on_step=False, on_epoch=True)
         self.log("val_labelwise_accuracy", labelwise_accuracy, prog_bar=True, on_step=False, on_epoch=True)
-        self.log("val_lr_accuracy", lr_accuracy, prog_bar=True, on_step=False, on_epoch=True)
+        return embedding, ordinal_labels
+
+    def validation_epoch_end(self, outputs):
+        all_embeddings = torch.cat([embedding for (embedding, ordinal_labels) in outputs], dim=0)  # iib, channel -> float32
+        all_ordinal_labels = torch.cat([ordinal_labels for (embedding, ordinal_labels) in outputs], dim=0)  # iib -> int
+        lr_accuracy_skl = get_logistic_regression_accuracy_skl(all_embeddings, all_ordinal_labels)
+        _, lr_accuracy_ours = get_logistic_regression_loss(all_embeddings, all_ordinal_labels)
+        self.log("val_lr_accuracy_skl", lr_accuracy_skl, prog_bar=True, on_step=False, on_epoch=True)
+        self.log("val_lr_accuracy_ours", lr_accuracy_ours, prog_bar=True, on_step=False, on_epoch=True)
+        print(f'lr_accuracy_skl = {lr_accuracy_skl:02f}')
+        print(f'lr_accuracy_ours = {lr_accuracy_ours:02f}')
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
@@ -105,7 +133,7 @@ def main():
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=256, shuffle=True, num_workers=4)
     val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=2048, shuffle=False, num_workers=4)
 
-    model = Model()
+    model = Model(lr_regularised=True)
 
     trainer = pl.Trainer(max_epochs=20, accelerator='gpu')
     trainer.fit(model, train_dataloaders=train_loader, val_dataloaders=val_loader)
