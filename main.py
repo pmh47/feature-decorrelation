6a1ad41b9d2831b0daa838f2d48c96c862f55716
Paul Henderson <paul@pmh47.net>
2023-02-08 03:06:50 +0000

    Initial work on logistic regression

--- main.py
+++ main.py
@@ -7,48 +7,22 @@ import torchvision.models
 import pytorch_lightning as pl
 
 
-# def get_slab_coordinates(xyzs, slab_inner_vertices, slab_outer_vertices):
-#
-#     # xyzs :: iib, xyz; these are the world-space coordinates we want to map to slab-space
-#     # slab_*_vertices :: iib, vertex-in-face, xyz
-#
-#     slab_vertices = torch.stack([slab_inner_vertices, slab_outer_vertices], dim=1)  # iib, inner/outer, vertex-in-face, xyz
-#
-#     def slab_to_world(uvw):
-#         u, v, w = uvw.unbind(dim=-1)  # each indexed by iib
-#         barycentric_interped = u[:, None, None] * slab_vertices[:, :, 0] + v[:, None, None] * slab_vertices[:, :, 1] + (1. - u - v)[:, None, None] * slab_vertices[:, :, 2]  # iib, inner/outer, xyz
-#         w_lerped = torch.lerp(barycentric_interped[:, 0], barycentric_interped[:, 1], 1. - w.unsqueeze(1))  # iib, xyz
-#         return w_lerped
-#
-#     def get_distances(uvw):
-#         # For the given slab-space coordinates, return how close the resulting world coordinates are to the targets along each axis
-#         candidate_world = slab_to_world(uvw)  # iib, xyz
-#         return torch.square(candidate_world - xyzs)  # iib, xyz
-#
-#     def batch_jacobian(f, x):
-#         # Assuming f(x) and x are both indexed by [iib, A], and that the i^th slice of f(x) depends only on the i^th slice of x, this
-#         # efficiently returns a batch of Jacobian matrices of f(x) wrt x, of shape [iib, A, A]
-#         # Inspired by https://discuss.pytorch.org/t/80771/5
-#         def sum_f(x):
-#             return f(x).sum(dim=0)
-#         return torch.autograd.functional.jacobian(sum_f, x, vectorize=True).permute(1, 0, 2)
-#
-#     # ** we should consider using multiple random restarts per iib, to mitigate non-convergence
-#     uvw_t = torch.full_like(xyzs, 0.5)  # iib, uvw
-#     # uvw_t = torch.rand_like(xyzs) * 0.2 + 0.4  # iib, uvw
-#
-#     newton_iterations = 8  # ** this should probably be a hyperparameter! and/or, we could do a proper convergence check
-#     for t in range(newton_iterations):
-#         # Do a batched Newton-Raphson iteration of the form x_{t+1} = x_t - J(x_t)^{-1} f(x_t), where f : R^3 -> R^3
-#         # maps slab to world coordinates and measures the resulting per-axis errors, and J is the jacobian of f
-#         distances = get_distances(uvw_t)  # iib, xyz
-#         jacobians = batch_jacobian(get_distances, uvw_t)  # iib, xyz, uvw
-#         jacobians_or_eyes = torch.where((distances == 0.).unsqueeze(-1), torch.eye(3).to(jacobians.device), jacobians)  # if x/y/z distance is zero, then relevant partial derivatives are also zero, leading to singular jacobian
-#         uvw_t += torch.lu_solve(-distances.unsqueeze(-1), *torch.lu(jacobians_or_eyes)).squeeze(-1)  # iib, uvw
-#
-#     converged = get_distances(uvw_t).mean(dim=-1) < slab_inner_vertices.std(dim=-2).mean(dim=-1) * 1.e-2  # i.e. require convergence to within approx. 1% of triangle extent
-#
-#     return uvw_t, converged
+def do_logistic_regression(embedding, labels):
+    # embedding :: iib, channel -> float32
+    # labels :: iib -> int
+    num_classes = 10
+    batch_size = embedding.shape[1]
+    model = nn.Linear(batch_size, num_classes).to(embedding.device)
+    def logits_and_loss():
+        logits = model(embedding)
+        return logits, F.cross_entropy(logits, labels)
+    lr = 4.e-2
+    for _ in range(20):
+        current_logits, current_loss = logits_and_loss()
+        print('loss:', current_loss.item(), '; accuracy:', (current_logits.argmax(dim=1) == labels).float().mean().item())
+        grads = torch.autograd.grad(current_loss, model.parameters(), create_graph=False)
+        for param, grad in zip(model.parameters(), grads):
+            param.data -= lr * grad
 
 
 class Model(pl.LightningModule):
@@ -73,6 +47,7 @@ class Model(pl.LightningModule):
     def training_step(self, batch, batch_idx):
         images, ordinal_labels, text_labels = batch
         embedding = self.fc(self.resnet(images.expand(-1, 3, -1, -1)))
+        do_logistic_regression(embedding, ordinal_labels)
         character_logits = self.char_decoder(embedding.unsqueeze(-1))  # iib, char-in-alphabet, char-in-seq
         loss = F.cross_entropy(character_logits, text_labels)
         return loss
